dd34016d22a256ec7b1ef210ba6166a698b76127
Renée Kooi <renee@kooi.me>
2018-01-24 16:27:04 +0100

    slice

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 module.exports = function Splicer (str) {
   var splices = []
-  return { splice: splice, toString: toString }
+  var self = { splice: splice, slice: slice, toString: toString }
+  return self
 
   function splice (start, end, value) {
     splices.push({ start: start, end: end, value: value })
+    return self
   }
 
-  function toString () {
+  function slice (start, end) {
     var result = ''
-    var last = 0
+    var last = start
     for (var i = 0; i < splices.length; i++) {
       var sp = splices[i]
-      // console.log('start', JSON.stringify(str.slice(last, sp.start)), 'value', JSON.stringify(sp.value))
+      if (end != null) {
+        if (sp.start >= end) return result + str.slice(last, end)
+      }
       result += str.slice(last, sp.start) + sp.value
       last = sp.end
     }
-    // console.log('end', JSON.stringify(str.slice(last)))
-    return result + str.slice(last)
+    return result + str.slice(last, end)
+  }
+
+  function toString () {
+    return slice(0)
   }
 }
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var Splicer = require('./')
 
 var str = Splicer('a b c d e')
-str.splice(2, 3, 'beep')
-str.splice(6, 7, 'boop')
+  .splice(2, 3, 'beep')
+  .splice(6, 7, 'boop')
 console.log(str.toString())
+console.log(str.slice(2, 8))
+console.log('a b c d e'.slice(2, 8))
